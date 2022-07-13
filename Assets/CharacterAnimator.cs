01b38bd8a6fac3414c4fb17fb9271db0d004accb
Eleanor Post <laneybird13@gmail.com>
2022-07-12 23:33:05 -0400

    My buddy and I can run circles around each other

--- Assets/CharacterAnimator.cs
+++ Assets/CharacterAnimator.cs
@@ -1,18 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class CharacterAnimator : MonoBehaviour
 {
+    private IAstarAI ai;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private int direction = 0;
+    private float maxSpeed = 10.0f;
+    private float minSpeed = 0.75f;
+
     // Start is called before the first frame update
     void Start()
     {
+        ai = GetComponent<IAstarAI>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    int CheckDirection( Vector2 velocity)
+    {
+        int direction;
+
+        float angle = Vector2.Dot(velocity.normalized, Vector2.up);
+
+        if (angle > .75)
+        {
+            direction = 0;
+        }
+        else if (angle > .25)
+        {
+            direction = 1;
+        }
+        else if (angle > -.25)
+        {
+            direction = 2;
+        }
+        else if (angle > -.75)
+        {
+            direction = 3;
+        }
+        else
+        {
+            direction = 4;
+        }
         
+        if (velocity.x > 0f)
+        {
+            direction = (8 - direction) % 8;
+        }
+
+        return direction;
+    }
+
+    void AnimateCharacter(Vector2 velocity)
+    {
+        string actionName = "";
+
+        if (true) // check if difference in velocity magnitude falls into skid zone? don't know. 
+        {
+            if (velocity.magnitude < minSpeed)
+            {
+                actionName = "Stand";
+            }
+            else
+            {
+                actionName = "Run"; //Later skid?
+                direction = CheckDirection(velocity);
+            }
+        }
+
+        actionName += "_" + direction;
+
+        animator.Play(actionName);
+        sprite.flipX = (direction > 4); // Only use if you mirror
+        //animator.speed = velocity.magnitude / maxSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AnimateCharacter(ai.velocity);
     }
 }
--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class CharacterController2D : MonoBehaviour
 {
+    IAstarAI ai;
+
     private Vector2 acceleration;
     private Vector2 velocity;
     private bool moveKey;
@@ -13,72 +16,10 @@ public class CharacterController2D : MonoBehaviour
     public float minSpeed = .01f;
     public float traction = .975f;
 
-    public Animator animator;
-    public SpriteRenderer sprite;
-
     // Start is called before the first frame update
     void Start()
     {
         velocity = new Vector2();
-    }
-
-    void CheckDirection()
-    {
-        float angle = Vector2.Dot(velocity.normalized, Vector2.up);
-
-        if (angle > .75)
-        {
-            direction = 0;
-        }
-        else if (angle > .25)
-        {
-            direction = 1;
-        }
-        else if (angle > -.25)
-        {
-            direction = 2;
-        }
-        else if (angle > -.75)
-        {
-            direction = 3;
-        }
-        else
-        {
-            direction = 4;
-        }
-        
-        if (velocity.x > 0f)
-        {
-            direction = (8 - direction) % 8;
-        }
-    }
-
-    void AnimateCharacter()
-    {
-        string actionName = "";
-
-        if (!moveKey)
-        {
-            if (velocity.magnitude < minSpeed)
-            {
-                actionName = "Stand";
-            }
-            else
-            {
-                actionName = "Run"; //Later skid?
-            }
-        }
-        else
-        {
-            actionName = "Run";
-            CheckDirection();
-        }
-
-        actionName += "_" + direction;
-
-        animator.Play(actionName);
-        sprite.flipX = (direction > 4); // Only use if you mirror
-        animator.speed = velocity.magnitude / maxSpeed;
     }
 
     // Update is called once per frame
@@ -103,7 +44,6 @@ public class CharacterController2D : MonoBehaviour
         }
 
         transform.position += (Vector3)velocity * Time.deltaTime;
-        AnimateCharacter();
     }
 
 
